201e4b01535844d1d5f157776eb8fd0c7cfae77a
Tianluhua <466469918@qq.com>
2018-04-25 09:55:00 +0800

    屏幕保持常亮

--- app/src/main/java/com/sat/satpic/base/AbstractMVPActivity.java
+++ app/src/main/java/com/sat/satpic/base/AbstractMVPActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.sat.satpic.Config;
@@ -29,6 +30,7 @@ public abstract class AbstractMVPActivity<V extends BaseView, P extends Abstract
         //在清单配置文件强制activity横屏导致启动应用慢的问题
         if (getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         if (mPresenter == null) {
             mPresenter = createPresenter();
